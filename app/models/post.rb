577c5e9786197107314e54d2e575d7c75574c245
Tsering Tsering <dtsering2@gmail.com>
2022-09-22 18:22:32 -0400

    commit tree d5ac190a2ee98915717c3e4c37ffc05b97d1823a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, allow_blank: false
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    validate :clickbait?
+    
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def clickbait?
+        if CLICKBAIT_PATTERNS.none? {|pattern| pattern.match? (title)}
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
